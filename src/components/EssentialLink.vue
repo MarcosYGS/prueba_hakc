a5a24937dd80d8223cfcf943e6cac7923eba8165
Marcos_yeyo <yeyomarcos@gmail.com>
2022-07-31 02:05:20 -0500

    instalando dependencias y plugin

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -19,31 +19,6 @@
   </q-item>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
 
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-
-    caption: {
-      type: String,
-      default: ''
-    },
-
-    link: {
-      type: String,
-      default: '#'
-    },
-
-    icon: {
-      type: String,
-      default: ''
-    }
-  }
-})
 </script>
