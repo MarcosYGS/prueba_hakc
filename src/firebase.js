234e9a236675ff4b396a8752056d4e4d1dad1ea6
Marcos_yeyo <yeyomarcos@gmail.com>
2022-08-01 00:23:17 -0500

    agregando configuracion de firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDhLb7hSBEJt0kLJyk35In9H5KEOCLfv0",
+  authDomain: "prueba-hackathon-42b01.firebaseapp.com",
+  projectId: "prueba-hackathon-42b01",
+  storageBucket: "prueba-hackathon-42b01.appspot.com",
+  messagingSenderId: "608915608088",
+  appId: "1:608915608088:web:53924c458591557ead296d"
+};
+
+
+const app = initializeApp(firebaseConfig);
